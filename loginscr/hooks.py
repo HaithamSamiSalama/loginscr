4d75a68e18e8350ae4234bded14c78ffbf06effd
Haitham Salama <haitham.salamaa@gmail.com>
2026-06-01 10:12:00 +0400

    Comment out web_include_css in hooks.py
    Comment out the web_include_css line in hooks.py.

--- loginscr/hooks.py
+++ loginscr/hooks.py
@@ -29,7 +29,7 @@ app_license = "mit"
 # app_include_js = "/assets/loginscr/js/loginscr.js"
 
 # include js, css files in header of web template
-web_include_css = "/assets/loginscr/css/loginscr.css"
+#web_include_css = "/assets/loginscr/css/loginscr.css"
 # web_include_js = "/assets/loginscr/js/loginscr.js"
 
 # include custom scss in every website theme (without file extension ".scss")
