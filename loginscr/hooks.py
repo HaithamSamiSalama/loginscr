8561240ab17bbaf3a9900d66e3335bd77024a208
Haitham Salama <haitham@Haithams-MacBook-Air.local>
2026-06-01 15:23:54 +0400

    update

--- loginscr/hooks.py
+++ loginscr/hooks.py
@@ -30,6 +30,7 @@ app_license = "mit"
 
 # include js, css files in header of web template
 #web_include_css = "/assets/loginscr/css/loginscr.css"
+web_include_css = "/assets/loginscr/css/loginscr.css"
 # web_include_js = "/assets/loginscr/js/loginscr.js"
 
 # include custom scss in every website theme (without file extension ".scss")
